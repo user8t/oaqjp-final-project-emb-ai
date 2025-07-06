944e25d4a5bc49121fbdce3eb4727791a0337b82
user8t <luffy18122003@gmail.com>
2025-07-06 09:54:48 -0400

    flask server and package setup

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from EmotionDetection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/emotionDetector', methods=['POST'])
+def emotion_detect_route():
+    text = request.form['text']
+    result = emotion_detector(text)
+
+    response_string = (
+        f"For the given statement, the system response is "
+        f"'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, "
+        f"'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. "
+        f"The dominant emotion is {result['dominant_emotion']}."
+    )
+
+    return response_string
+
+if __name__ == '__main__':
+    app.run(debug=True)
